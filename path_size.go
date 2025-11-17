6c1282da4e0ab730010a838e0dccaeba758eec2e
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-11-17 14:24:46 +0300

    add get size func

--- path_size.go
+++ path_size.go
@@ -1,6 +1,10 @@
 package pathsize
 
-import "github.com/urfave/cli/v3"
+import (
+	"github.com/urfave/cli/v3"
+	"os"
+	"path/filepath"
+)
 
 func NewApp() *cli.Command {
 	app := &cli.Command{
@@ -10,4 +14,36 @@ func NewApp() *cli.Command {
 	}
 
 	return app
+}
+
+func GetSize(path string) (int64, error) {
+	info, err := os.Lstat(path)
+	if err != nil {
+		return 0, err
+	}
+
+	if !info.IsDir() {
+		return info.Size(), nil
+	}
+
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return 0, err
+	}
+
+	var size int64
+
+	for _, entry := range entries {
+		childPath := filepath.Join(path, entry.Name())
+		childInfo, err := os.Lstat(childPath)
+
+		if err != nil {
+			return 0, err
+		}
+
+		if childInfo.Mode().IsRegular() {
+			size += childInfo.Size()
+		}
+	}
+	return size, nil
 }
