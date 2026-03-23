31be5a98ed3ce0b0ba341e0c0ca4d17bd3667846
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2026-03-23 15:27:47 +0300

    add tests for size calc

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
+
 [![Actions Status](https://github.com/spoddub/go-project-242/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/spoddub/go-project-242/actions)
+[![tests](https://github.com/spoddub/CLI-Disk-Analyzer/actions/workflows/tests.yml/badge.svg?branch=main)](https://github.com/OWNER/REPO/actions/workflows/tests.yml)
---
+++ path_size_test.go
@@ -0,0 +1,29 @@
+package code
+
+import "testing"
+
+func TestGetPathSize_File(t *testing.T) {
+	got, err := GetPathSize("testdata/data.csv", false, true, false)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "416B"
+
+	if got != want {
+		t.Errorf("file data.csv size should be %s, got %s", want, got)
+	}
+}
+
+func TestGetPathSize_Dir(t *testing.T) {
+	got, err := GetPathSize("testdata/dir1", false, true, false)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "257B"
+
+	if got != want {
+		t.Errorf("dir testdata/dir1 size is %s, got %s", want, got)
+	}
+}
